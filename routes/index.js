4e169586f6d237ca3967f0d16d7a6b3f00dd1c29
Spooky <smlsun@gmail.com>
2012-11-13 15:52:05 +0800

    add facebook twitter button

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,14 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: '雲端農業' });
 
   //res.redirect("index.html");
 };
 
+
 exports.buy = function(req, res){
-  res.render('buy', { title: 'Express' });
+  res.render('buy', { title: '購買產品' });
 
   //res.redirect("index.html");
 };
